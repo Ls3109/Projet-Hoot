e72922cc0b96b0c24fa66c9b9e8a976c48024c2b
Ls3109 <louissenac09@gmail.com>
2022-05-03 23:06:03 +0200

    login sign up tweet et like dislike

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+    def profile
+        @userid = params[:user_id]
+        @userinfo = User.find(params[:user_id])
+        @usercount = @userinfo.tweets
+        @tweets=Tweet.all
+    end
+
+    def show
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root 'tweets#index'
+  
+  devise_for :users
+  
+  
+  resources :tweets do
+    member do
+      put "like", to: "tweets#like"
+      put "dislike", to: "tweets#dislike"
+    end
+  end
+
+  resources :users do
+    get 'profile'
+  end
+
+end
