f5af32f91fe1a1bf943b2c83c220b029497ee0de
Enzo Parriel <enzo.parriel@gmail.com>
2022-05-04 11:34:37 +0200

    logout fonctionnel

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,13 @@
 class TweetsController < ApplicationController
-
-    before_action :authenticate_user!, exept: [:index, :show]
-
+    before_action :authenticate_user!
     def index
         @tweets = Tweet.all
     end
-       
+
     def new
         @tweet = Tweet.new
     end
-       
+
     def create
         @tweet = Tweet.new(tweet_params)
         @tweet.user_id = current_user.id
@@ -25,19 +23,19 @@ class TweetsController < ApplicationController
         @tweet.destroy
         redirect_to '/', :notice => "Your tweet has been deleted"
       end
-    
+
     def like
         @tweet = Tweet.find(params[:id])
         @tweet.liked_by current_user
         redirect_to '/'
     end
-      
+
     def dislike
         @tweet = Tweet.find(params[:id])
         @tweet.disliked_by current_user
         redirect_to '/'
     end
-       
+
     private
     def tweet_params
         params.require(:tweet).permit(:body)
